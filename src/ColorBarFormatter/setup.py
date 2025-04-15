037bd30c6a41aeab9dcc92700cdc875701423d90
kaeshev <kaeshev@gmail.c0om>
2025-04-15 23:47:41 +0000

    added entrypoint

--- src/ColorBarFormatter/setup.py
+++ src/ColorBarFormatter/setup.py
@@ -19,7 +19,7 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
+        'console_scripts': ['set_colors = ColorBarFormatter.set_colors:main',
         ],
     },
 )
