0b5968b6cb4ccdb63c1bf67713a55e921c145709
Rica <pagadc@rpi.edu>
2025-04-16 03:18:57 +0800

    updates

--- src/robotControl/setup.py
+++ src/robotControl/setup.py
@@ -1,24 +1,28 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'robotControl'
 
 setup(
     name=package_name,
+    version='0.0.0',
     packages=[package_name],
     data_files=[
-        # This ensures the package is registered with ROS2
+
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        # This installs the package.xml
+
         ('share/' + package_name, ['package.xml']),
-        # Include launch files in the installation
-        ('share/' + package_name + '/launch', ['launch/robotControl.launch.py']),
+
+        (os.path.join('share',package_name,'launch'),
+	 glob(os.path.join('launch','*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='your_email@example.com',
-    description='Robot control using colored signal detection.',
-    license='Apache-2.0',
+    maintainer='Chacrica Pagadala',
+    maintainer_email='pagadc@rpi.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
