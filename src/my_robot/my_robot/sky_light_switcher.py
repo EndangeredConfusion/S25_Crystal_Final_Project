64637e5821d881c1bc981a737e4d9651d92a07bb
carsoe2 <91489348+carsoe2@users.noreply.github.com>
2025-04-18 15:22:22 -0400

    correcting light model

--- src/my_robot/my_robot/sky_light_switcher.py
+++ src/my_robot/my_robot/sky_light_switcher.py
@@ -10,25 +10,26 @@ class RedLightGreenLight(Node):
         super().__init__('light_switcher')
         self.current_color = 'green'
         self.timer = self.create_timer(5.0, self.switch_light)
-        self.model_path = os.path.expanduser('~/ros2_ws/src/my_robot/models')
+        self.model_path = os.path.expanduser('~/S25_Crystal_Final_Project/src/my_robot/models')
 
     def switch_light(self):
         # Delete the current light (if it exists)
-        subprocess.run(['ros2', 'service', 'call', '/delete_entity', 'gazebo_msgs/srv/DeleteEntity',
-                        f'{{name: "{self.current_color}_light"}}'], shell=True)
-        time.sleep(0.5)
+        command = f'ros2 service call /delete_entity gazebo_msgs/srv/DeleteEntity "{{name: \\"{self.current_color}_light\\"}}"'
+        subprocess.run(command, shell=True)
 
         # Switch color
         self.current_color = 'red' if self.current_color == 'green' else 'green'
         model_file = os.path.join(self.model_path, f'{self.current_color}_light', 'model.sdf')
 
         # Spawn the new light
-        subprocess.run([
-            'ros2', 'run', 'gazebo_ros', 'spawn_entity.py',
-            '-file', model_file,
-            '-entity', f'{self.current_color}_light',
-            '-x', '0', '-y', '0', '-z', '3'
-        ], shell=True)
+        command = (
+            f'ros2 run gazebo_ros spawn_entity.py '
+            f'-file {model_file} '
+            f'-entity {self.current_color}_light '
+            f'-x 0 -y 0 -z 3'
+        )
+
+        subprocess.run(command, shell=True)
 
 def main():
     rclpy.init()
